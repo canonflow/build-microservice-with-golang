3a3a5c36eb69dc06dd8e8b1136ab7ec1b927e597
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-12-03 18:19:36 +0700

    feat(redis): Add redis
    - refactor error handler on app.Start() with greatful shutdown
    - Add goroutine to server the server

--- application/app.go
+++ application/app.go
@@ -3,16 +3,20 @@ package application
 import (
 	"context"
 	"fmt"
+	"github.com/redis/go-redis/v9"
 	"net/http"
+	"time"
 )
 
 type App struct {
 	route http.Handler
+	rdb   *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		route: loadRoutes(),
+		rdb:   redis.NewClient(&redis.Options{}),
 	}
 
 	return app
@@ -23,9 +27,58 @@ func (app *App) Start(ctx context.Context) error {
 		Addr:    ":3000",
 		Handler: app.route,
 	}
-	err := server.ListenAndServe()
+
+	// Check Redis Connection
+	err := app.rdb.Ping(ctx).Err()
+
 	if err != nil {
-		return fmt.Errorf("Failed to start server: %w", err)
+		return fmt.Errorf("Failed to connect to redis: %w", err)
 	}
+
+	// Grateful shutdown the redis if there is an error
+	defer func() {
+		if err := app.rdb.Close(); err != nil {
+			fmt.Printf("Failed to close redis connection")
+		}
+	}()
+
+	fmt.Println("Server is running ...")
+
+	// Make channel
+	/* ===== 1 specifies the buffer size of the channel =====
+	- The channel can hold 1 value in its buffer before it blocks further sends.
+	- If a value is sent to the channel and the buffer is full,
+	  the send operation (channel <- value) will block until there’s space in the buffer
+	  (i.e., until a receiver takes the value).
+	- If a value is received from the channel and the buffer is empty,
+	  the receive operation (<-channel) will block until a value is available.
+	*/
+	channel := make(chan error, 1)
+
+	// Init the goroutines
+	go func() {
+		// Start the Server
+		err = server.ListenAndServe()
+		if err != nil {
+			// Publish the value into the channel
+			channel <- fmt.Errorf("Failed to start server: %w", err)
+		}
+		// Close the channel
+		close(channel)
+	}()
+
+	// Will block our code's execution until it receives a value of the channel is closed
+	// Select -> similar to switch statement, but for channel
+	select {
+	case err = <-channel:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// If the shutdown exceeds 10 seconds, it will be forcibly stopped.
+		return server.Shutdown(timeout)
+	}
+
 	return nil
 }
--- main.go
+++ main.go
@@ -4,13 +4,23 @@ import (
 	"context"
 	"fmt"
 	"github.com/canonflow/build-microservice-with-golang/application"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	app := application.New()
-	err := app.Start(context.TODO())
+
+	// Root level context
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+
+	err := app.Start(ctx)
+	// Kill the root and its children context
+	// defer -> will run at the end of the file
+	defer cancel()
 
 	if err != nil {
 		fmt.Println("Failed to start application:", err)
 	}
+
 }
